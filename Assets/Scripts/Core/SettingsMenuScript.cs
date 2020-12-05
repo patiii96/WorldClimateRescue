469c76ba6a12967d771eb472cb418b35a05addcf
Adi Huseinbegovic <adi.huseinbegovic@student.tugraz.at>
2020-12-05 11:14:33 +0100

    audio settings added

---
+++ Assets/Scripts/Core/SettingsMenuScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SettingsMenuScript : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AudioToggleClicked(bool isOn)
+    {
+       AudioListener.volume = Convert.ToInt32(isOn);
+    }
+
+    public void AudioSliderChanged(float value)
+    {
+        AudioListener.volume = value;
+    }
+}
